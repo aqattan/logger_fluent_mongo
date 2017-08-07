a741c36ed938806c013985379af0addbd85949e9
Alkattan <aqattan@yahoo.com>
2017-08-07 16:24:47 -0500

    changing the quotation

--- app/controllers/api/v1/messages_controller.rb
+++ app/controllers/api/v1/messages_controller.rb
@@ -2,6 +2,6 @@ class Api::V1::MessagesController < ApplicationController
   def message
       message = params[:message]
       MessagesJob.perform_async(message)
-      render json: "success"
+      render json: 'Success'
   end
 end
